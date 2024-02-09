4079d06b88ad6e9308607a9244b5618302a9e99f
janvi18 <sjanvi9333@gmail.com>
2024-02-09 10:02:25 +0530

    1.0

--- demo.js
+++
@@ -1,9 +0,0 @@
-function showDetails(name, description) {
-    document.getElementById('cake-name').textContent = name;
-    document.getElementById('cake-description').textContent = description;
-    document.getElementById('cake-details-modal').style.display = 'block';
-}
-
-function hideDetails() {
-    document.getElementById('cake-details-modal').style.display = 'none';
-}
